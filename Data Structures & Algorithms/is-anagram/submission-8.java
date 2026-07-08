4726202a96e3ddfb718dfd6c9a6222d50072802e
krish kumar <146408926+krisjscott@users.noreply.github.com>
2026-07-08 16:15:00 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        List<Character> set1 = new ArrayList<>();
+        List<Character> set2 = new ArrayList<>(); 
+
+        for(char c: s.toCharArray()){
+            set1.add(c);
+        }    
+        for(char a: t.toCharArray()){
+            set2.add(a);
+        }
+        
+        Collections.sort(set1);
+        Collections.sort(set2);
+
+        if(set1.equals(set2)) return true;
+        else return false; 
+    }
+}
