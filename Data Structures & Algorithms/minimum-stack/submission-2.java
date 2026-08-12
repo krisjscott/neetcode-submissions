a02dfc7baabce5abb164d1fbffe09f5d63da2988
krish kumar <146408926+krisjscott@users.noreply.github.com>
2026-08-12 13:19:32 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,28 @@
+class MinStack {
+    private Stack<int[]> stack;
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if(stack.isEmpty()){
+            stack.push(new int[]{val, val});
+        }
+        else{
+            int curMin = stack.peek()[1];
+            stack.push(new int[]{val, Math.min(val, curMin)});
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
